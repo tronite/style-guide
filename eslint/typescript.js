623494cfdc09afeabe1b3fb9e04ece374f601b72
Richard Solomou <richard@solomou.io>
2023-12-10 11:00:46 +0200

    fix typescript overrides

--- eslint/typescript.js
+++ eslint/typescript.js
@@ -10,18 +10,13 @@ module.exports = {
     {
       files: TYPESCRIPT_FILES,
       extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript', 'prettier'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
     },
   ],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-duplicate-enum-values': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-  },
 };
